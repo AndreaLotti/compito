a8e80b4aeac398d8675cf4c1a70aad5892abe10b
Leonardo Giustiniani <andrealotti88@gmail.com>
2019-06-04 12:19:26 +0200

    giusto

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int k=1;k<=10;k++)//ciclo da 1 a 10
+    {
+        cout << k << endl;//mostro la variabile contatore
+    }
+<<<<<<< HEAD
+    cout << "master" << endl;
+=======
+    cout << "branch" << endl;
+>>>>>>> FIX_BUG
+    system ("PAUSE");
+}
